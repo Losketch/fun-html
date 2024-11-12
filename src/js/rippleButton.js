e51c7e03f37118c8d3ad68a43ca8ed19cf28a607
gollllll <0@a>
2024-11-12 21:59:26 +0800

    添加了按钮动画。

---
+++ src/js/rippleButton.js
@@ -0,0 +1,23 @@
+const buttons = document.querySelectorAll('button');
+
+for (let button of buttons) {
+  button.addEventListener('click', function (e) {
+    const button = e.currentTarget;
+
+    const ripple = document.createElement('span');
+    const rect = button.getBoundingClientRect();
+
+    const size = Math.max(rect.width, rect.height);
+    ripple.style.width = ripple.style.height = `${size}px`;
+    ripple.style.left = `${e.clientX - rect.left - size / 2}px`;
+    ripple.style.top = `${e.clientY - rect.top - size / 2}px`;
+    
+    ripple.classList.add('ripple');
+
+    button.insertBefore(ripple, button.firstChild);
+    
+    ripple.addEventListener('animationend', () => {
+        ripple.remove();
+    });
+  });
+}
