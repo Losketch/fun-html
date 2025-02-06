c2caf230e7716612fe0fd6404e256f15f598808c
-PT_AxH_RV- <0@a>
2025-02-06 20:29:46 +0800

    修使用了 Web Worker 优化了 randStr.

---
+++ src/js/filterDefinedCharacters.js
@@ -0,0 +1,66 @@
+import { definedCharacterList } from '../data/DefinedCharacterList.js';
+
+const workerScript = `
+  self.onmessage = function(event) {
+    const { characters, definedCharacterList } = event.data;
+    const definedSet = new Set(definedCharacterList);
+    const filteredCharacters = characters.filter(i => definedSet.has(i));
+    self.postMessage(filteredCharacters);
+  };
+`;
+
+const workerBlob = new Blob([workerScript], { type: 'application/javascript' });
+const workerUrl = URL.createObjectURL(workerBlob);
+
+function chunkArray(array, chunkSize) {
+  const chunks = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
+
+function runWorker(worker, data) {
+  return new Promise((resolve, reject) => {
+    worker.addEventListener('message', event => {
+      resolve(event.data);
+    });
+
+    worker.addEventListener('error', error => {
+      reject(error);
+    });
+
+    worker.postMessage(data);
+  });
+}
+
+export async function filterDefinedCharacters(characters, numWorkers) {
+  const chunkSize = Math.ceil(characters.length / numWorkers);
+  const chunks = chunkArray(characters, chunkSize);
+
+  const workers = [];
+  const promises = [];
+
+  for (let i = 0; i < numWorkers; i++) {
+    const worker = new Worker(workerUrl);
+    workers.push(worker);
+
+    const promise = runWorker(worker, {
+      characters: chunks[i],
+      definedCharacterList
+    });
+    promises.push(promise);
+  }
+
+  try {
+    const results = await Promise.all(promises);
+
+    const filteredCharacters = results.flat();
+    return filteredCharacters;
+  } catch (error) {
+    console.error('Error in Web Worker:', error);
+    throw error;
+  } finally {
+    workers.forEach(worker => worker.terminate());
+  }
+}
