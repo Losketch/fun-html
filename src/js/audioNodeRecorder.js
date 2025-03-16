7159c075d2d195f9d0fba426141a236a6e6d06d6
-PT_AxH_RV- <0@a>
2025-03-16 21:28:23 +0800

    拍格式化代码。

--- src/js/audioNodeRecorder.js
+++ src/js/audioNodeRecorder.js
@@ -2,55 +2,62 @@ import mp3EncoderWorker from './workers/mp3Encoder.worker.js';
 
 class AudioNodeRecorder {
   constructor(audioNode, nextNode, options = {}) {
-    this.audioNode = audioNode
-    this.nextNode = nextNode
-    this.context = audioNode.context
-    this.options = options
-    this.worker = new mp3EncoderWorker()
-    this.worker.onmessage = this.handleWorkerMessage.bind(this)
+    this.audioNode = audioNode;
+    this.nextNode = nextNode;
+    this.context = audioNode.context;
+    this.options = options;
+    this.worker = new mp3EncoderWorker();
+    this.worker.addEventListener(
+      'message',
+      this.handleWorkerMessage.bind(this)
+    );
 
-    const inputSampleRate = this.context.sampleRate
-    const outputSampleRate = options.sampleRate || inputSampleRate
-    const bitRate = options.bitRate || 128
-    const numChannels = options.numChannels || 1
+    const inputSampleRate = this.context.sampleRate;
+    const outputSampleRate = options.sampleRate || inputSampleRate;
+    const bitRate = options.bitRate || 128;
+    const numChannels = options.numChannels || 1;
 
     this.worker.postMessage({
       cmd: 'init',
       data: { numChannels, inputSampleRate, outputSampleRate, bitRate }
-    })
+    });
 
-    this.processor = this.context.createScriptProcessor(4096, numChannels, numChannels)
-    this.processor.onaudioprocess = e => {
-      if (!this.isRecording) return
-      const data = []
+    this.processor = this.context.createScriptProcessor(
+      4096,
+      numChannels,
+      numChannels
+    );
+    this.processor.addEventListener('audioprocess', e => {
+      if (!this.isRecording) return;
+      const data = [];
       for (let i = 0; i < numChannels; i++) {
-        data.push(e.inputBuffer.getChannelData(i))
+        data.push(e.inputBuffer.getChannelData(i));
       }
-      this.worker.postMessage({ cmd: 'encode', data })
-    }
+      this.worker.postMessage({ cmd: 'encode', data });
+    });
 
-    audioNode.connect(this.processor)
-    this.processor.connect(nextNode)
-    this.isRecording = false
-    this.onComplete = options.onComplete
+    audioNode.connect(this.processor);
+    this.processor.connect(nextNode);
+    this.isRecording = false;
+    this.onComplete = options.onComplete;
   }
 
   handleWorkerMessage(e) {
     if (e.data.cmd === 'complete') {
-      const blob = new Blob(e.data.data, { type: 'audio/mp3' })
-      this.onComplete?.(blob)
+      const blob = new Blob(e.data.data, { type: 'audio/mp3' });
+      this.onComplete?.(blob);
     }
   }
 
   startRecording() {
-    this.isRecording = true
+    this.isRecording = true;
   }
 
   stopRecording() {
-    this.isRecording = false
-    this.worker.postMessage({ cmd: 'stop' })
+    this.isRecording = false;
+    this.worker.postMessage({ cmd: 'stop' });
   }
-  
+
   destruction() {
     if (this.audioNode && this.processor) {
       this.audioNode.disconnect(this.processor);
@@ -60,13 +67,13 @@ class AudioNodeRecorder {
     }
 
     if (this.processor) {
-      this.processor.onaudioprocess = null;
+      this.processor.addEventListener('audioprocess', null);
       this.processor.disconnect();
       this.processor = null;
     }
 
     if (this.worker) {
-      this.worker.onmessage = null;
+      this.worker.addEventListener('message', null);
       this.worker.terminate();
       this.worker = null;
     }
--- src/js/tools/morse.js
+++ src/js/tools/morse.js
@@ -5,7 +5,7 @@ import '../m3ui.js';
 import '../changeHeader.js';
 import '../iframeColorSchemeSync.js';
 
-import AudioNodeRecorder from '../audioNodeRecorder.js'
+import AudioNodeRecorder from '../audioNodeRecorder.js';
 
 const wpmSlider = document.getElementById('wpm-slider');
 const wordIntervalSlider = document.getElementById('wi-slider');
@@ -28,40 +28,43 @@ gainNode.gain.setValueAtTime(0, audioContext.currentTime);
 
 oscillator.start(audioContext.currentTime);
 
-function playSound(soundSequence, onComplete=null) {  
+function playSound(soundSequence, onComplete = null) {
   let playerTimer = undefined;
 
   function _play(soundSequence) {
     const sound = soundSequence.shift();
     if (sound === undefined) {
-      if (onComplete) onComplete()
-      return
+      if (onComplete) onComplete();
+      return;
     }
-    
-    const {interval, duration} = sound
-    
+
+    const { interval, duration } = sound;
+
     function start() {
-      gainNode.gain.setValueAtTime(0.2, audioContext.currentTime)
-      playerTimer = setTimeout(end, duration)
+      gainNode.gain.setValueAtTime(0.2, audioContext.currentTime);
+      playerTimer = setTimeout(end, duration);
     }
-    
+
     function end() {
       playerTimer = undefined;
       gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime);
-      _play(soundSequence)
+      _play(soundSequence);
     }
-    
-    if (interval == 0) {start()}
-    else {playerTimer = setTimeout(start, interval)}
+
+    if (interval === 0) {
+      start();
+    } else {
+      playerTimer = setTimeout(start, interval);
+    }
   }
-  
-  _play(soundSequence)
-  
+
+  _play(soundSequence);
+
   return () => {
-    clearTimeout(playerTimer)
-    playerTimer = undefined
+    clearTimeout(playerTimer);
+    playerTimer = undefined;
     gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime);
-  }
+  };
 }
 
 String.prototype.toArray = function () {
@@ -72,27 +75,32 @@ String.prototype.toArray = function () {
     arr.push(codePoint);
   }
   return arr;
-}
+};
 
 function logBase(x, base) {
   if (x <= 0 || base <= 0 || base === 1) return NaN;
   return Math.log(x) / Math.log(base);
 }
 
-function encodeMorse(text, wpm=20, wordInterval=3, dashDotLenRatio=3) {
+function encodeMorse(text, wpm = 20, wordInterval = 3, dashDotLenRatio = 3) {
   const unitTime = 1.2 / wpm;
   const soundSequence = [];
   let isWordStart;
   let isFirstLetter = true;
   for (const i of text.toArray()) {
     for (let j = Math.floor(logBase(i, 2)); j >= 0; j--) {
-      const interval = unitTime * (isWordStart ? 3 : 1) * (isFirstLetter ? 0 : 1) * 1000
-      const duration = ((i >> j & 1) * (dashDotLenRatio - 1) + 1) * unitTime * 1000;
+      const interval =
+        unitTime *
+        (isWordStart ? wordInterval : 1) *
+        (isFirstLetter ? 0 : 1) *
+        1000;
+      const duration =
+        (((i >> j) & 1) * (dashDotLenRatio - 1) + 1) * unitTime * 1000;
       soundSequence.push({ interval, duration });
       isFirstLetter = false;
       isWordStart = false;
     }
-    isWordStart = true
+    isWordStart = true;
   }
 
   return soundSequence;
@@ -103,16 +111,32 @@ let stopper;
 encodeButton.addEventListener('click', () => {
   if (stopper) stopper();
 
-  oscillator.frequency.setValueAtTime(+freqSlider.value, audioContext.currentTime);
-  const soundSequence = encodeMorse(input.value, +wpmSlider.value, +wordIntervalSlider.value, +dashDotLenRatioSlider.value)
-  stopper = playSound(soundSequence)
+  oscillator.frequency.setValueAtTime(
+    +freqSlider.value,
+    audioContext.currentTime
+  );
+  const soundSequence = encodeMorse(
+    input.value,
+    +wpmSlider.value,
+    +wordIntervalSlider.value,
+    +dashDotLenRatioSlider.value
+  );
+  stopper = playSound(soundSequence);
 });
 
 downloadButton.addEventListener('click', () => {
   if (stopper) stopper();
-  
-  oscillator.frequency.setValueAtTime(+freqSlider.value, audioContext.currentTime);
-  const soundSequence = encodeMorse(input.value, +wpmSlider.value, +wordIntervalSlider.value, +dashDotLenRatioSlider.value)
+
+  oscillator.frequency.setValueAtTime(
+    +freqSlider.value,
+    audioContext.currentTime
+  );
+  const soundSequence = encodeMorse(
+    input.value,
+    +wpmSlider.value,
+    +wordIntervalSlider.value,
+    +dashDotLenRatioSlider.value
+  );
   const recorder = new AudioNodeRecorder(gainNode, audioContext.destination, {
     bitRate: 128,
     onComplete: blob => {
@@ -122,9 +146,11 @@ downloadButton.addEventListener('click', () => {
       a.download = `telegram_${Date.now()}.mp3`;
       a.click();
     }
-  })
-  recorder.startRecording()
-  stopper = playSound(soundSequence, () => recorder.stopRecording())
+  });
+  recorder.startRecording();
+  stopper = playSound(soundSequence, () => recorder.stopRecording());
 });
 
-stopButton.addEventListener('click', () => {if (stopper) stopper()});
+stopButton.addEventListener('click', () => {
+  if (stopper) stopper();
+});
