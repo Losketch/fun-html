4767ae62e31561505a22063040ef5ae296053da7
-PT_AxH_RV- <140041025+1-PT-AxH-RV-0@users.noreply.github.com>
2025-06-07 23:08:50 +0800

    修正「欧甘文字转换器」中的一处错误。
    将错误的「ᚎ」改成「ᚏ」。

--- src/js/pages/ogham.js
+++ src/js/pages/ogham.js
@@ -14,54 +14,56 @@ const missingCharDialogText = missingCharDialog.querySelector('#text');
 const missingCharDialogClose = missingCharDialog.querySelector('#close');
 
 const oghamLettersMap = {
-  'ᚊ': /(\(k\))|q/ig,
-  'ᚕ': /(\(ea\))|k|x/ig,
-  'ᚖ': /\(oi\)/ig,
-  'ᚗ': /\(ui\)/ig,
-  'ᚘ': /\(io\)/ig,
-  'ᚙ': /\(ae\)/ig,
-  'ᚍ': /\(ng\)/ig,
-  'ᚚ': /p/ig,
-  'ᚁ': /b/ig,
-  'ᚂ': /l/ig,
-  'ᚃ': /v|f/ig,
-  'ᚄ': /s/ig,
-  'ᚅ': /n/ig,
-  'ᚆ': /h/ig,
-  'ᚇ': /d/ig,
-  'ᚈ': /t/ig,
-  'ᚉ': /c/ig,
-  'ᚋ': /m/ig,
-  'ᚌ': /g/ig,
-  'ᚎ': /z/ig,
-  'ᚎ': /r/ig,
-  'ᚐ': /a|á/ig,
-  'ᚑ': /o|ó/ig,
-  'ᚒ': /u|ú/ig,
-  'ᚓ': /e|é/ig,
-  'ᚔ': /i|í/ig,
+  ᚊ: /(\(k\))|q/gi,
+  ᚕ: /(\(ea\))|k|x/gi,
+  ᚖ: /\(oi\)/gi,
+  ᚗ: /\(ui\)/gi,
+  ᚘ: /\(io\)/gi,
+  ᚙ: /\(ae\)/gi,
+  ᚍ: /\(ng\)/gi,
+  ᚚ: /p/gi,
+  ᚁ: /b/gi,
+  ᚂ: /l/gi,
+  ᚃ: /v|f/gi,
+  ᚄ: /s/gi,
+  ᚅ: /n/gi,
+  ᚆ: /h/gi,
+  ᚇ: /d/gi,
+  ᚈ: /t/gi,
+  ᚉ: /c/gi,
+  ᚋ: /m/gi,
+  ᚌ: /g/gi,
+  ᚎ: /z/gi,
+  ᚏ: /r/gi,
+  ᚐ: /a|á/gi,
+  ᚑ: /o|ó/gi,
+  ᚒ: /u|ú/gi,
+  ᚓ: /e|é/gi,
+  ᚔ: /i|í/gi,
   ' ': / /g
-}
+};
 
 convertButton.addEventListener('click', () => {
   let text = original.value;
-  
+
   for (const oghamLetter in oghamLettersMap) {
     const regex = oghamLettersMap[oghamLetter];
     text = text.replace(regex, oghamLetter);
   }
-  
+
   for (const char of text) {
     if (Object.hasOwnProperty.call(oghamLettersMap, char)) continue;
-    missingCharDialogText.innerText = `欧甘文字中没有字符 “${char}” 的对应。`
+    missingCharDialogText.innerText = `欧甘文字中没有字符 “${char}” 的对应。`;
     missingCharDialog.show();
-    
+
     return;
   }
-  
-  if (featherMarks.checked) text = `᚛${text}᚜`
-  
-  ogham.value = text
-})
 
-missingCharDialogClose.addEventListener('click', () => missingCharDialog.close());
+  if (featherMarks.checked) text = `᚛${text}᚜`;
+
+  ogham.value = text;
+});
+
+missingCharDialogClose.addEventListener('click', () =>
+  missingCharDialog.close()
+);
